b02afe42e774e8283624bbc7d2cd9dd489f71aca
Andrey Pogorelov <namtydapro@gmail.com>
2020-03-14 21:06:27 +0300

    second version func

--- controllers/git.js
+++ controllers/git.js
@@ -1,5 +1,7 @@
 const { spawn } = require('child_process');
 const path = require('path');
+const fs = require('fs');
+
 function gitClone() {
   const git = spawn("git", ["clone", "https://github.com/necolas/normalize.css.git"]);
   git.stderr.on('data', err => {
@@ -14,23 +16,43 @@ function gitClone() {
 
 
 function getCommitInfo() {
-  let buff = Buffer.alloc(0);
-  const git = spawn('git', ['log', '--pretty=format:%H %cn %s'], {cdw: path.join(__dirname, '../normalize.css')});
-  console.log(path.join(__dirname, '../normalize.css'))
+  // Проверяю наличие папки
+  fs.stat(path.join(__dirname, '../', 'normalize.css'), (err, stat) => {
+    if (err) {
+      return console.log('Ошибка про проверки папки', err);
+    }
+    
+    let buff = Buffer.alloc(0);
+    let result;
+    process.chdir(path.join(__dirname, '../', 'normalize.css'));
 
-  git.stderr.on('data', err => {
-    console.log(err.toString('UTF-8'));
-  });
+    const git = spawn('git', ['log', '-2', '--pretty=format:%H %cn %s']);
+    console.log(path.join(__dirname, '../normalize.css'))
 
-  git.stdout.on('data', data => {
-    buff = Buffer.concat([buff, data])
-  });
+    git.stderr.on('data', err => {
+      console.log(err.toString('UTF-8'));
+    });
 
-  git.on('close', code => {
-    console.log(`Это код завершения процесса получения логов ${code}`);
-    const result = buff.toString().split('\n');
-    console.log(result)
+    git.stdout.on('data', data => {
+      buff = Buffer.concat([buff, data])
+    });
+
+    git.on('close', code => {
+      console.log(`Это код завершения процесса получения логов ${code}`);
+      const res = buff.toString().split('\n');
+      result = res.map((el, i) => el.split(' '));
+      console.log(result);
+    });
   });
 }
 getCommitInfo();
 module.exports = { gitClone, getCommitInfo }
+
+
+// const log = fs.createWriteStream('../log/log.txt', {flags: 'a'});
+// const err = fs.createWriteStream('../log/logErr.txt', {flags: 'a'});
+// const ss = spawan(...).stdio: [process.stdin, log, err]
+
+// ss.stdout.pipe(logStream)
+
+//git log -2 --pretty=format:'%H % cn %s'
