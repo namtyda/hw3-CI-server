dcc0538fafb58d697bea948f91f6e1072eefc072
Andrey Pogorelov <namtydapro@gmail.com>
2020-04-22 17:53:13 +0300

    api.ts

---
+++ ci-client/src/api/api.ts
@@ -0,0 +1,98 @@
+import ax from 'axios';
+
+const axios = ax.create({
+  baseURL: 'http://localhost:3000/api',
+  timeout: 10000,
+});
+
+export interface addQueue {
+  commitHash: string;
+}
+export interface responseAddQueue {
+  commitMessage: string;
+  commitHash: string;
+  branchName: string;
+  authorName: string;
+}
+
+export interface Config {
+  repoName: string;
+  buildCommand: string;
+  mainBranch: string;
+  period: number;
+}
+export enum Status {
+  Waiting = 'Waiting',
+  InProgress = 'InProgress',
+  Success = 'Success',
+  Fail = 'Fail',
+  Canceled = 'Canceled'
+}
+export interface getBuilds<T> {
+  id: string;
+  configurationId: string;
+  buildNumber: number;
+  commitMessage: string;
+  commitHash: string;
+  branchName: string;
+  authorName: string;
+  status: T;
+  start: string;
+  duration: number;
+}
+
+export interface getConfig {
+  id: string;
+  repoName: string;
+  buildCommand: string;
+  mainBranch: string;
+  period: number;
+}
+
+export const api = {
+  getConfig() {
+    return axios.get<getConfig>('/settings')
+      .then(res => res)
+      .catch(err => console.log(err));
+  },
+  getBuildsList(data: number) {
+    return axios.get<getBuilds<Status>>('/builds', {
+      params: {
+        limit: data
+      }
+    })
+      .then(res => res)
+      .catch(err => console.log(err));
+  },
+
+  getDetailsBuild(buildId: string) {
+    return axios.get<getBuilds<Status>>(`/builds/${buildId}`)
+      .then(res => res)
+      .catch(err => console.log(err));
+  },
+
+  postAddQueue(data: addQueue) {
+    return axios.post<responseAddQueue>(`/builds/${data.commitHash}`, {
+      timeout: 200000
+    })
+      .then(res => res)
+      .catch(err => err);
+  },
+
+  getLogs(buildId: string) {
+    return axios.get<string>(`/builds/${buildId}/logs`)
+      .then(res => res)
+      .catch(err => console.log(err));
+  },
+
+  postSaveSettings(data: Config) {
+    return axios.post('/settings', {
+      repoName: data.repoName,
+      buildCommand: data.buildCommand,
+      mainBranch: data.mainBranch,
+      period: Number(data.period)
+    })
+      .then(res => res)
+      .catch(err => console.log(err));
+  }
+}
